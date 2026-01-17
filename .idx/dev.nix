049b64839264d051f9626f9e4714799a807053a4
Iceman8911 <wuchijss3@gmail.com>
2026-01-17 22:21:55 +0000

    refactor: move to monorepo format

---
+++ .idx/dev.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-25.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.bun
+    pkgs.lazygit
+    pkgs.turbo
+    pkgs.git-lfs
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    SOME_ENV_VAR = "hello";
+  };
+
+}
