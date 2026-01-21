2e80479cbc1543d32f56dd4e8dd15a51b23c7d81
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-21 19:44:47 +0100

    chore: fix issues preventing turbo testing

--- apps/docs/src/entry-client.tsx
+++ apps/docs/src/entry-client.tsx
@@ -1,4 +1,8 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
-mount(() => <StartClient />, document.getElementById("app")!);
+const root = document.getElementById("app");
+
+if (!root) throw Error("Root element not found");
+
+mount(() => <StartClient />, root);
