adf22a2f027053bd498b4779d91f5f3350063f41
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-06 05:16:34 +0100

    chore(library): add polyfill package as dep

--- apps/library/bunup.config.ts
+++ apps/library/bunup.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "bunup";
 const config = defineConfig({
 	entry: ["src/index.ts"],
 	format: ["esm", "cjs"],
-	noExternal: ["@packages/serializer", "@packages/string-compression"],
+	noExternal: [
+		"@packages/serializer",
+		"@packages/string-compression",
+		"@packages/polyfills",
+	],
 });
 
 export default config as ReturnType<typeof defineConfig>;
